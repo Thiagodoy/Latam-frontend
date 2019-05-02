103c67a6aebccabe7527e41ed006560661391312
Luis Dias <luis.maisnet@gmail.com>
2019-05-02 10:56:32 -0300

    plugin autocomplete

---
+++ web-data-quality/src/pages/file/ItemTemplate.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    
+    <span>{{ item.name }}</span>
+ 
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: { required: true },
+    searchText: { required: true }
+  }
+}
+</script>
