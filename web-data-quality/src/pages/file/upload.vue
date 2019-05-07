d745ca271575d9e170d9a8db0d85b145dbf6f724
Thiagodoy <thiagodoy@hotmail.com>
2019-05-07 10:42:46 -0300

    Criação da mensagem para erro no server do sftp e aws

--- web-data-quality/src/pages/file/upload.vue
+++ web-data-quality/src/pages/file/upload.vue
@@ -110,7 +110,7 @@
                                 v-for="(v,i) in filesUploads" 
                                 :processFile="false" 
                                 :uploadAws="true" 
-                                :uploadFtp="false" 
+                                :uploadFtp="true" 
                                 :key="i" 
                                 :fileInput="v" 
                                 :index="i" 
